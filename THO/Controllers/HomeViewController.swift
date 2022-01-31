d47cb1c481e3ed16dbe41bf156c4c91ccf6778e5
Dan Akmat <akmatdan@gmail.com>
2022-01-31 15:34:27 +0600

    Setup and creating Controllers

--- THO/Controllers/HomeViewController.swift
+++ THO/Controllers/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  THO
 //
 //  Created by Daniil Akmatov on 31/1/22.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
