a7f979d7694f3e1fe9de1ead8a09d4b2366c8b4c
Dan Akmat <akmatdan@gmail.com>
2022-01-31 15:24:57 +0600

    Initial Commit

---
+++ THO/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  THO
+//
+//  Created by Daniil Akmatov on 31/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .link
+    }
+
+
+}
+
