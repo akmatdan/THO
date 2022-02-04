cba74d53871adbdb02ca8033744d1c0f79000bdb
Dan Akmat <akmatdan@gmail.com>
2022-02-05 02:00:35 +0600

    Update Controllers
    PayWallView and SignIn

---
+++ THO/Other/Extensions.swift
@@ -0,0 +1,35 @@
+//
+//  Extensions.swift
+//  THO
+//
+//  Created by Daniil Akmatov on 5/2/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    var width: CGFloat {
+        frame.size.width
+    }
+    
+    var height: CGFloat {
+        frame.size.height
+    }
+    
+    var left: CGFloat {
+        frame.origin.x
+    }
+    
+    var right: CGFloat {
+        left + width
+    }
+    
+    var top: CGFloat {
+        frame.origin.y
+    }
+    
+    var bottom: CGFloat {
+        top + height
+    }
+}
